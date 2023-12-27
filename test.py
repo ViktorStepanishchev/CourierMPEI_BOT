3a9546ee23fa0766f97c2ceaccad2a598c916095
byvs <132465630+ViktorStepanishchev@users.noreply.github.com>
2023-12-28 01:44:35 +0300

    Delete test.py

--- test.py
+++
@@ -1,8 +0,0 @@
-import openpyxl
-
-book = openpyxl.open("example.xlsx", read_only=True)
-sheet = book.active
-for row in range(1, 1000):
-    if not (sheet.cell(row=row, column=1).value is None):
-        print('TRUE')
-book.close()
