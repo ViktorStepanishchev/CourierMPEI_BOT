d6afb58054bae701d07dc89fe72c35f85cb9120c
byvs <v.stepanishchev@bk.ru>
2025-03-23 12:35:52 +0300

    add generating order_id

--- handlers/user/create_order_handler.py
+++ handlers/user/create_order_handler.py
@@ -5,13 +5,24 @@ from aiogram.fsm.context import FSMContext
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from database.sessions.user_session.order_session import (orm_get_customer_info,
-                                                          orm_add_customer)
+                                                          orm_add_customer,
+                                                          orm_get_costumer_attr)
+from random import randint
 
 from common.texts import user_text
 from common.states import CreateOrder
 from kbds.reply_kbds.user_reply_kbds import get_send_phone
 
 create_order_router = Router()
+
+async def gen_order_id(session: AsyncSession):
+    info = await orm_get_costumer_attr(session = session,
+                                       attr = 'order_id')
+
+    while True:
+        order_id = randint(10000, 99999)
+        if order_id not in info:
+            return order_id
 
 @create_order_router.callback_query(F.data == 'order')
 async def f_start_create_order(callback: CallbackQuery, state: FSMContext, session: AsyncSession):
@@ -20,7 +31,7 @@ async def f_start_create_order(callback: CallbackQuery, state: FSMContext, sessi
     if customer_data is not None:
         order_id = customer_data.order_id
         await callback.message.edit_text(text = user_text['order_is_already_exists'].format(order_id),
-                                reply_markup=ReplyKeyboardRemove())
+                                reply_markup=None)
         return
 
     await state.set_state(CreateOrder.order_text)
@@ -43,6 +54,9 @@ async def f_get_order_photo(message: Message, state: FSMContext, session: AsyncS
     data = await state.get_data()
 
     await orm_add_customer(session = session,
-                           user_id = data.get('user_id'),
+                           user_id = message.from_user.id,
                            username = message.from_user.username,
-                           order_id = 12345)
+                           order_id = await gen_order_id(session),
+                           order_text = data['order_text'],
+                           order_photo = photo)
+
