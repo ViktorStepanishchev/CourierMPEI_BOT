80f6e2d9c19860ce37d2771734393b6b25892fdc
byvs <v.stepanishchev@bk.ru>
2025-03-29 17:35:23 +0300

    admin answer user: add texts

--- handlers/admin/answer_user_question_handler.py
+++ handlers/admin/answer_user_question_handler.py
@@ -2,8 +2,11 @@ from aiogram import F, Router
 from aiogram.fsm.context import FSMContext
 from aiogram.types import CallbackQuery, Message
 
+from common.filters.username_filter import UsernameFilter
 from common.states import AnswerMessageAdministration
 from common.filters.chat_type_filter import ChatTypeFilter
+from common.texts.admin_texts import admin_help_text
+from common.texts.user_texts import user_text
 from config import CHAT_ADMIN
 
 answer_user_question_router = Router()
@@ -14,17 +17,13 @@ answer_user_question_router.message.filter(ChatTypeFilter(chat_type=['group', 's
 async def f_admin_press_button_for_answer_user_question(callback: CallbackQuery, state: FSMContext):
     question_data = callback.data.split("_")
     question_user_id, question_username = int(question_data[-2]), question_data[-1]
-    if callback.from_user.username is None:
-        await callback.bot.send_message(
-            text = 'Выдайте себе username для ответа на вопрос пользователя',
-            chat_id=callback.from_user.id,
-        )
-        return
+
     await state.update_data(user_id=question_user_id)
     await state.update_data(user_username=question_username)
     await state.set_state(AnswerMessageAdministration.admin_msg_id)
     await callback.message.edit_reply_markup(reply_markup=None)
-    await callback.message.edit_text(text=f"Админ @{callback.from_user.username} отвечает на вопрос @{question_username}")
+    await callback.message.edit_text(text=admin_help_text['admin_answering_user'].format(admin_username=callback.from_user.username,
+                                                                                         user_username=question_username))
 
 @answer_user_question_router.message(AnswerMessageAdministration.admin_msg_id)
 async def f_admin_answer_user_question(message: Message, state: FSMContext):
@@ -33,7 +32,7 @@ async def f_admin_answer_user_question(message: Message, state: FSMContext):
     await state.clear()
 
     await message.bot.send_message(
-        text=f"Уважаемый @{user_username}! Ответ администрации: \n",
+        text=user_text['admin_answer'],
         chat_id=user_id,
     )
 
@@ -42,4 +41,6 @@ async def f_admin_answer_user_question(message: Message, state: FSMContext):
         from_chat_id=CHAT_ADMIN,
         message_id=admin_msg_id,
     )
-    await message.answer(text=f'Ответ отправлен @{user_username}')
+    await message.answer(text=admin_help_text['admin_answer_user'].format(
+        admin_username=message.from_user.username,
+        user_username=user_username))
