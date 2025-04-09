7b4b3cd2f5f499f4cd59372258454b2777d1a16f
byvs <v.stepanishchev@bk.ru>
2025-04-10 00:36:23 +0300

    Courier: refactor & view order info

---
+++ handlers/user/courier_handlers/view_take_order_handler.py
@@ -0,0 +1,43 @@
+from aiogram import F, Router
+from aiogram.types import CallbackQuery, Message
+from aiogram.fsm.context import FSMContext
+from scripts.regsetup import description
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from common.texts.user_texts import courier_text, user_text
+from kbds.inline_kbds.user_inline_kbds import orders_kbds
+from database.sessions.user_session.order_session import orm_get_order
+
+view_take_order_router = Router()
+
+@view_take_order_router.callback_query(F.data.startswith('courier_'))
+async def f_view_orders(callback: CallbackQuery, session: AsyncSession):
+    callback_data = callback.data.split("_")
+    if callback_data == 'courier': page=0
+    else: page = int(callback.data.split("_")[-1])
+
+    await callback.message.edit_text(text=courier_text['view_orders'],
+                                     reply_markup = await orders_kbds(session, page))
+
+@view_take_order_router.callback_query(F.data.startswith('order_'))
+async def f_view_order(callback: CallbackQuery, session: AsyncSession):
+    callback_data = callback.data.split("_")
+    order_id = int(callback_data[-1])
+
+    order_data = await orm_get_order(session=session, order_id=order_id)
+    if order_data is None:
+        # обработка в случае редактируемого/удалённого заказа
+        ...
+        return
+    # добавить обработку в случае, если заказ уже взят другим курьером
+
+    view_order_text = courier_text['view_order_first_info'].format(order_id=order_data.order_id,
+                                                                description=order_data.order_text)
+    if order_data.order_photo:
+        await callback.message.edit_reply_markup(reply_markup=None)
+        await callback.message.answer_photo(photo=order_data.order_photo,
+                                            caption=view_order_text,
+                                            reply_markup = None) # доделать клавиатуру
+        return
+    await callback.message.edit_text(text=view_order_text,
+                                     reply_markup = None) # доделать клавиатуру
